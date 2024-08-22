dc4de02b516153d2334ff8b3d3e6ab46d2fff6ba
JasonMar1 <iasonasmarinopoulos@gmail.com>
2024-08-22 19:17:00 +0300

    Alpha version

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_info.db'
 # # }
 app.config['SECRET_KEY'] = '085559c59aae537ebb4942f9'
 
-DATABASE_URL = os.environ['DATABASE_URL']
+try:
+    DATABASE_URL = 'postgres://uecsanlvvmajn6:p97f31bc94b829247b353b4933a858e22b372bddd8cebdab009480e8a409a13bc@c724r43q8jp5nk.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d2kv80mrpj37s8'
+except KeyError:
+    print("Error: The DATABASE_URL environment variable is not set.")
+
+
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
--- app/create_app.py
+++ app/create_app.py
@@ -1,5 +1,7 @@
 from app.routes import app
-
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
+    app.run(debug=False)
