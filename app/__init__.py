ae4008f45dac6bff444355a9b1da50588423158c
JasonMar1 <iasonasmarinopoulos@gmail.com>
2024-08-22 20:38:49 +0300

    Alpha version

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 import psycopg2
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_info.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://uecsanlvvmajn6:p97f31bc94b829247b353b4933a858e22b372bddd8cebdab009480e8a409a13bc@c724r43q8jp5nk.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d2kv80mrpj37s8'
 # # app.config['SQLALCHEMY_BINDS'] = {
 # #     'user_info': 'sqlite:///user_info.db'
 # # }
